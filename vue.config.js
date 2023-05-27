fafaa44e2beb979bc831bfe4b52184477f2c220d
散人 <2248780501@qq.com>
2023-05-27 17:03:52 +0800

    第一次提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+
+
+// element-plus按需自动引入
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //注意plugins放在外面会报错，必须加上configureWebpack
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ]
+  }
 })
+
